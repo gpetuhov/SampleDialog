5cea2378cd28506d5c8e865021397b89c78f9ee6
Gennadiy Petuhov <g3pmail@gmail.com>
2017-02-28 11:38:30 +0400

    Add date picker to date dialog

--- app/src/main/java/com/gpetuhov/android/sampledialog/DatePickerFragment.java
+++ app/src/main/java/com/gpetuhov/android/sampledialog/DatePickerFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 // Fragment for the dialog with date picker
 public class DatePickerFragment extends DialogFragment {
@@ -15,8 +17,13 @@ public class DatePickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        // Create new AlertDialog
+        // Create view with date picker
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
+        // Create new AlertDialog with this view
         AlertDialog dateDialog = new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_dialog_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
